26f209f3f033858bc5f062d1efb2643b292c8346
Bobby Bouwmann <bobbybouwmann@gmail.com>
2017-01-16 08:25:52 +0100

    Update default Page.php

--- tests/Browser/Pages/Page.php
+++ tests/Browser/Pages/Page.php
@@ -7,6 +7,16 @@ use Laravel\Dusk\Page as BasePage;
 class Page extends BasePage
 {
     /**
+     * Get the URL for the page.
+     *
+     * @return string
+     */
+    public function url()
+    {
+        return '';
+    }
+
+    /**
      * Get the global element shortcuts for the site.
      *
      * @return array
